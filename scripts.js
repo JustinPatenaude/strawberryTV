6a127258f46239c209644600c6f3f573a7dc3a32
Justin Patenaude <justin@boldapps.net>
2016-01-22 09:18:22 -0600

    More commits

--- scripts.js
+++ scripts.js
@@ -1,55 +1,146 @@
 (function($, window, document) {
-    var movieList = $('.movie-list');
-    $('.search-form').on("submit", function(e) {
-        console.log('hello');
-        var form = $(this);
+    var url = 'https://api.themoviedb.org/3/',
+        mode = 'search/multi',
+        input,
+        movieName,
+        key = '&api_key=f9060b0815c54d485f9548181eef3918',
+        img_url = "http://image.tmdb.org/t/p/w500",
+        no_poster = "no-poster.png";
+
+    $('.search-form').on('submit', function(e) {
+        var input = $('input[name="q"]').val(),
+            movieName = encodeURI(input);
         e.preventDefault();
+        $('.movie-list').html('');
         $.ajax({
-            url: "http://imdb.wemakesites.net/api/search",
-            data: form.serialize(),
-            crossDomain: true,
-            dataType: "jsonp",
+            type: 'GET',
+            url: url + mode + '?query='+movieName + key,
+            async: false,
+            jsonpCallback: 'testing',
+            contentType: 'application/json',
+            dataType: 'jsonp',
             success: function(json) {
-                window.console.log(json);
-                var titles_to_get = json.data.results.titles.length;
-                var titles_loaded = 1;
-                console.log(json.data.results.titles.length);
-                $.each(json.data.results.titles, function(index){
-                    $.ajax({
-                        url: "http://imdb.wemakesites.net/api/" + this.id,
-                        crossDomain: true,
-                        dataType: "jsonp",
-                        success: function(json2) {
-                            window.console.log(json2);
-                            $('.movie-list').append('<div class="movie">\
-                                <div class="movie-image" style="background-image: url('+json2.data.image+')"></div>\
-                                <div class="movie-title"><h2>'+json2.data.title+'</h2></div>\
-                                <div class="movie-description"><p>'+json2.data.description+'</p></div>\
-                                </div>');
-                            if(titles_loaded==titles_to_get){
-                                check_images();
-                            }
-                            titles_loaded++;
+                console.log(json);
+                $.each(json.results, function(show){
+                    if(this.media_type != "person"){
+                        if(this.poster_path != null){
+                            var thePoster = img_url+this.poster_path
                         }
-                    });
-                })
+                        else {
+                            var thePoster = no_poster;
+                        }
+                        if(this.media_type == 'tv'){
+                            var theIcon = '<i class="fa fa-tv"></i>',
+                                theTitle = this.name,
+                                theDate = this.first_air_date.split('-'),
+                                theDate = theDate[0];
+                        }
+                        else {
+                            var theIcon = '<i class="fa fa-film"></i>',
+                                theTitle = this.title,
+                                theDate = this.release_date.split('-'),
+                                theDate = theDate[0];
+                        }
+                        $('.movie-list').append('<div class="movie">\
+                            <div class="movie-icon">'+theIcon+'</div>\
+                            <div class="movie-image" style="background-image: url('+thePoster+')"></div>\
+                            <div class="movie-title"><h2>'+theTitle+'<span class="movie-date">'+theDate+'</span></h2></div>\
+                            <div class="movie-description"><p>'+this.overview+'</p></div>\
+                            </div>');
+                    }
+                });
             },
-            error: function(err){
-                console.log(err);
+            error: function(e) {
+                console.log(e.message);
             }
         });
     });
 
-    function check_images(){
-        $('.movie-image img').each(function(){
-            var imgHeight = $(this).height();
-            var imgWidth = $(this).width();
-            var ratio = imgHeight / imgWidth;
-            console.log('ratio values: '+imgHeight+' + '+imgWidth);
-            console.log('ratio: '+ratio);
-            if(ratio < 1.45){
-                $(this).attr('src', 'http://placehold.it/214x320');
+    $('#login').click(function(){
+        login();
+    });
+
+    var OAUTHURL    =   'https://accounts.google.com/o/oauth2/auth?';
+    var VALIDURL    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
+    var SCOPE       =   'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive';
+    var CLIENTID    =   '397849418785-4aqfsk8kpeuu36eapp0k5cqkk8di4u9o.apps.googleusercontent.com';
+    var REDIRECT    =   'http://justinpatenaude.github.io/strawberryTV/'
+    var LOGOUT      =   'http://accounts.google.com/Logout';
+    var TYPE        =   'token';
+    var _url        =   OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
+    var acToken;
+    var tokenType;
+    var expiresIn;
+    var user;
+    var loggedIn    =   false;
+
+    function login() {
+        var win         =   window.open(_url, "windowname1", 'width=800, height=600'); 
+
+        var pollTimer   =   window.setInterval(function() { 
+            try {
+                console.log(win.document.URL);
+                if (win.document.URL.indexOf(REDIRECT) != -1) {
+                    window.clearInterval(pollTimer);
+                    var url =   win.document.URL;
+                    acToken =   gup(url, 'access_token');
+                    tokenType = gup(url, 'token_type');
+                    expiresIn = gup(url, 'expires_in');
+                    win.close();
+
+                    validateToken(acToken);
+                }
+            } catch(e) {
             }
+        }, 500);
+    }
+
+    function validateToken(token) {
+        $.ajax({
+            url: VALIDURL + token,
+            data: null,
+            success: function(responseText){  
+                getUserInfo();
+                loggedIn = true;
+                $('#loginText').hide();
+                $('#logoutText').show();
+            },  
+            dataType: "jsonp"  
         });
     }
+
+    function getUserInfo() {
+        $.ajax({
+            url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
+            data: null,
+            success: function(resp) {
+                user    =   resp;
+                console.log(user);
+                $('#uName').text('Welcome ' + user.name);
+                $('#imgHolder').attr('src', user.picture);
+            },
+            dataType: "jsonp"
+        });
+    }
+
+    //credits: http://www.netlobo.com/url_query_string_javascript.html
+    function gup(url, name) {
+        name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+        var regexS = "[\\#&]"+name+"=([^&#]*)";
+        var regex = new RegExp( regexS );
+        var results = regex.exec( url );
+        if( results == null )
+            return "";
+        else
+            return results[1];
+    }
+
+    function startLogoutPolling() {
+        $('#loginText').show();
+        $('#logoutText').hide();
+        loggedIn = false;
+        $('#uName').text('Welcome ');
+        $('#imgHolder').attr('src', 'none.jpg');
+    }
+
 }(window.jQuery, window, document));
