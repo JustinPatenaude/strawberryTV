be926393f2be8e2cf34af3aa87eaa7c432c2ada0
Justin Patenaude <justin@boldapps.net>
2016-01-22 14:00:36 -0600

    Testing

--- scripts.js
+++ scripts.js
@@ -1,73 +1,3 @@
-// $('.spreadsheet').submit(function(e){
-//     var public_spreadsheet_url = $('.spreadsheet-input').val();
-//     init(public_spreadsheet_url);
-//     e.preventDefault;
-// });
-
-
-// function init(public_spreadsheet_url) {
-// Tabletop.init( { key: public_spreadsheet_url,
-//                  callback: showInfo,
-//                  simpleSheet: true } )
-// }
-
-// function showInfo(data, tabletop) {
-// alert("Successfully processed!")
-// console.log(data);
-// }
-
-// var key = "1MVAORx23e9ttJOBv6Idn_6m2I5EeOddryW1MArtIxqM",
-//     apiURL = "http://spreadsheets.google.com/feeds/cells/" + key + "/1/public/values",
-//     apiURL = apiURL + "?alt=json";
-// $.getJSON(apiURL).then(function(data) {
-//     console.log(data);
-//     getData(data);
-// });
-// function getData(resp) {
-//     //Functions to get row and column from cell reference
-//     var get_row = function(str) { return /[0-9]{1,10}/.exec(str)*1 };
-//     var get_col = function(str) { return /[A-Z]{1,2}/.exec(str) };
- 
-//     var data = [];
-//     var cols = ["A"];
-//     var cells = resp.feed.entry;
-//     var column_headers = {};
-//     var number_of_cols = 0;
-//     var number_of_rows = get_row(cells[cells.length-1].title.$t);
-//     var row = 1;
-//     var _row = {};
- 
-//     //Get column headers
-//     for (i=0; i
-//         var _cell_ref = cells[i].title.$t;
-//         var _column = get_col(_cell_ref);
-//         if (_cell_ref == "A2") {
-//             break;
-//         }
-//         else {
-//             column_headers[_column] = cells[i].content.$t;
-//             number_of_cols = 0;
-//         };
-//     };
- 
-//     //Iterate cells and build data object
-//     for (i=number_of_cols; i
-//         var cell_ref = cells[i].title.$t,
-//             col = get_col(cell_ref);
-//         if (row < get_row(cell_ref)) {
-//             //new row
-//             data.push(_row);
-//             _row = {};
-//             row++;
-//         }
-//         _row[column_headers[col]] = cells[i].content.$t;
-//     }
-//     data.push(_row);
-//     data.shift();
-//     return data;
-// }
-
-
 // var OAUTHURL    =   'https://accounts.google.com/o/oauth2/auth?';
 // var VALIDURL    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
 // var SCOPE       =   'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive';
@@ -82,144 +12,144 @@
 // var user;
 // var loggedIn    =   false;
 
-// (function($, window, document) {
-//     var url = 'https://api.themoviedb.org/3/',
-//         mode = 'search/multi',
-//         input,
-//         movieName,
-//         key = '&api_key=f9060b0815c54d485f9548181eef3918',
-//         img_url = "http://image.tmdb.org/t/p/w500",
-//         no_poster = "no-poster.png";
+(function($, window, document) {
+    var url = 'https://api.themoviedb.org/3/',
+        mode = 'search/multi',
+        input,
+        movieName,
+        key = '&api_key=f9060b0815c54d485f9548181eef3918',
+        img_url = "http://image.tmdb.org/t/p/w500",
+        no_poster = "no-poster.png";
 
-//     $('.search-form').on('submit', function(e) {
-//         var input = $('input[name="q"]').val(),
-//             movieName = encodeURI(input);
-//         e.preventDefault();
-//         $('.movie-list').html('');
-//         $.ajax({
-//             type: 'GET',
-//             url: url + mode + '?query='+movieName + key,
-//             async: false,
-//             jsonpCallback: 'testing',
-//             contentType: 'application/json',
-//             dataType: 'jsonp',
-//             success: function(json) {
-//                 console.log(json);
-//                 $.each(json.results, function(show){
-//                     if(this.media_type != "person"){
-//                         if(this.poster_path != null){
-//                             var thePoster = img_url+this.poster_path
-//                         }
-//                         else {
-//                             var thePoster = no_poster;
-//                         }
-//                         if(this.media_type == 'tv'){
-//                             var theIcon = '<i class="fa fa-tv"></i>',
-//                                 theTitle = this.name,
-//                                 theDate = this.first_air_date.split('-'),
-//                                 theDate = theDate[0];
-//                         }
-//                         else {
-//                             var theIcon = '<i class="fa fa-film"></i>',
-//                                 theTitle = this.title,
-//                                 theDate = this.release_date.split('-'),
-//                                 theDate = theDate[0];
-//                         }
-//                         $('.movie-list').append('<div class="movie">\
-//                             <div class="movie-icon">'+theIcon+'</div>\
-//                             <div class="movie-image" style="background-image: url('+thePoster+')"></div>\
-//                             <div class="movie-title"><h2>'+theTitle+'<span class="movie-date">'+theDate+'</span></h2></div>\
-//                             <div class="movie-description"><p>'+this.overview+'</p></div>\
-//                             </div>');
-//                     }
-//                 });
-//             },
-//             error: function(e) {
-//                 console.log(e.message);
-//             }
-//         });
-//     });
+    $('.search-form').on('submit', function(e) {
+        var input = $('input[name="q"]').val(),
+            movieName = encodeURI(input);
+        e.preventDefault();
+        $('.movie-list').html('');
+        $.ajax({
+            type: 'GET',
+            url: url + mode + '?query='+movieName + key,
+            async: false,
+            jsonpCallback: 'testing',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.log(json);
+                $.each(json.results, function(show){
+                    if(this.media_type != "person"){
+                        if(this.poster_path != null){
+                            var thePoster = img_url+this.poster_path
+                        }
+                        else {
+                            var thePoster = no_poster;
+                        }
+                        if(this.media_type == 'tv'){
+                            var theIcon = '<i class="fa fa-tv"></i>',
+                                theTitle = this.name,
+                                theDate = this.first_air_date.split('-'),
+                                theDate = theDate[0];
+                        }
+                        else {
+                            var theIcon = '<i class="fa fa-film"></i>',
+                                theTitle = this.title,
+                                theDate = this.release_date.split('-'),
+                                theDate = theDate[0];
+                        }
+                        $('.movie-list').append('<div class="movie">\
+                            <div class="movie-icon">'+theIcon+'</div>\
+                            <div class="movie-image" style="background-image: url('+thePoster+')"></div>\
+                            <div class="movie-title"><h2>'+theTitle+'<span class="movie-date">'+theDate+'</span></h2></div>\
+                            <div class="movie-description"><p>'+this.overview+'</p></div>\
+                            </div>');
+                    }
+                });
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+        });
+    });
 
-//     $('#login').click(function(){
-//         login();
-//     });
+    // $('#login').click(function(){
+    //     login();
+    // });
 
-//     $('#logout').click(function(){
-//         myIFrame.location='https://www.google.com/accounts/Logout';
-//         startLogoutPolling();
-//         return false;
-//     });
+    // $('#logout').click(function(){
+    //     myIFrame.location='https://www.google.com/accounts/Logout';
+    //     startLogoutPolling();
+    //     return false;
+    // });
 
-//     function login() {
-//         var win         =   window.open(_url, "windowname1", 'width=800, height=600'); 
+    // function login() {
+    //     var win         =   window.open(_url, "windowname1", 'width=800, height=600'); 
 
-//         var pollTimer   =   window.setInterval(function() { 
-//             try {
-//                 console.log(win.document.URL);
-//                 if (win.document.URL.indexOf(REDIRECT) != -1) {
-//                     window.clearInterval(pollTimer);
-//                     var url =   win.document.URL;
-//                     acToken =   gup(url, 'access_token');
-//                     tokenType = gup(url, 'token_type');
-//                     expiresIn = gup(url, 'expires_in');
-//                     win.close();
+    //     var pollTimer   =   window.setInterval(function() { 
+    //         try {
+    //             console.log(win.document.URL);
+    //             if (win.document.URL.indexOf(REDIRECT) != -1) {
+    //                 window.clearInterval(pollTimer);
+    //                 var url =   win.document.URL;
+    //                 acToken =   gup(url, 'access_token');
+    //                 tokenType = gup(url, 'token_type');
+    //                 expiresIn = gup(url, 'expires_in');
+    //                 win.close();
 
-//                     validateToken(acToken);
-//                 }
-//             } catch(e) {
-//             }
-//         }, 500);
-//     }
+    //                 validateToken(acToken);
+    //             }
+    //         } catch(e) {
+    //         }
+    //     }, 500);
+    // }
 
-//     function validateToken(token) {
-//         $.ajax({
-//             url: VALIDURL + token,
-//             data: null,
-//             success: function(responseText){  
-//                 getUserInfo();
-//                 loggedIn = true;
-//                 $('.login').hide();
-//                 $('.logout').show();
-//             },  
-//             dataType: "jsonp"  
-//         });
-//     }
+    // function validateToken(token) {
+    //     $.ajax({
+    //         url: VALIDURL + token,
+    //         data: null,
+    //         success: function(responseText){  
+    //             getUserInfo();
+    //             loggedIn = true;
+    //             $('.login').hide();
+    //             $('.logout').show();
+    //         },  
+    //         dataType: "jsonp"  
+    //     });
+    // }
 
-//     function getUserInfo() {
-//         $.ajax({
-//             url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
-//             data: null,
-//             success: function(resp) {
-//                 console.log(resp);
-//                 user    =   resp;
-//                 console.log(user);
-//                 var url = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full?access_token=' + acToken;
-//                 $.get(url, function(data) {
-//                     console.log(data);
-//                 });
-//             },
-//             dataType: "jsonp"
-//         });
-//     }
+    // function getUserInfo() {
+    //     $.ajax({
+    //         url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
+    //         data: null,
+    //         success: function(resp) {
+    //             console.log(resp);
+    //             user    =   resp;
+    //             console.log(user);
+    //             var url = 'https://spreadsheets.google.com/feeds/spreadsheets/private/full?access_token=' + acToken;
+    //             $.get(url, function(data) {
+    //                 console.log(data);
+    //             });
+    //         },
+    //         dataType: "jsonp"
+    //     });
+    // }
 
-//     //credits: http://www.netlobo.com/url_query_string_javascript.html
-//     function gup(url, name) {
-//         name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-//         var regexS = "[\\#&]"+name+"=([^&#]*)";
-//         var regex = new RegExp( regexS );
-//         var results = regex.exec( url );
-//         if( results == null )
-//             return "";
-//         else
-//             return results[1];
-//     }
+    // //credits: http://www.netlobo.com/url_query_string_javascript.html
+    // function gup(url, name) {
+    //     name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    //     var regexS = "[\\#&]"+name+"=([^&#]*)";
+    //     var regex = new RegExp( regexS );
+    //     var results = regex.exec( url );
+    //     if( results == null )
+    //         return "";
+    //     else
+    //         return results[1];
+    // }
 
-//     function startLogoutPolling() {
-//         $('.login').show();
-//         $('.logout').hide();
-//         loggedIn = false;
-//         $('#uName').text('Welcome ');
-//         $('#imgHolder').attr('src', 'none.jpg');
-//     }
+    // function startLogoutPolling() {
+    //     $('.login').show();
+    //     $('.logout').hide();
+    //     loggedIn = false;
+    //     $('#uName').text('Welcome ');
+    //     $('#imgHolder').attr('src', 'none.jpg');
+    // }
 
-// }(window.jQuery, window, document));
+}(window.jQuery, window, document));
